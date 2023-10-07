ac3113a46d8305f83211b5d5cfb194a13d7fdbd9
hyunmo.ahn <hyunmo.ahn@linecorp.com>
2023-10-08 02:04:40 +0900

    feat: image example

--- apps/css-playground/src/components/image/image.stories.tsx
+++ apps/css-playground/src/components/image/image.stories.tsx
@@ -1,24 +1,60 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { FlowerImage } from '.';
+import {
+  ShadowImage,
+  ShadeImage,
+  FrameImage,
+  BlendImage,
+  LetterImage,
+} from '.';
+import image12 from '~/assets/images/pic012.jpg';
+import image14 from '~/assets/images/pic014.jpg';
+import image37Logo from '~/assets/images/pic037-logo.png';
+import image54 from '~/assets/images/pic054.jpg';
 
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
-
-const meta: Meta<typeof FlowerImage> = {
-  component: FlowerImage,
+const meta: Meta<typeof ShadowImage> = {
+  component: ShadowImage,
   title: 'Image',
 };
 export default meta;
-type Story = StoryObj<typeof FlowerImage>;
+type Story = StoryObj<typeof ShadowImage>;
 
 export const Flower: Story = {
-  args: {},
+  args: {
+    src: image12,
+  },
 };
 
-// export const Heading: Story = {
-//   args: {},
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement);
-//     expect(canvas.getByText(/Welcome to App!/gi)).toBeTruthy();
-//   },
-// };
+export const Cafe: Story = {
+  args: {
+    src: image14,
+  },
+  render: (args) => (
+    <ShadeImage {...(args as React.ImgHTMLAttributes<HTMLImageElement>)} />
+  ),
+};
+
+export const Frame: Story = {
+  args: {
+    src: image14,
+  },
+  render: (args) => (
+    <FrameImage {...(args as React.ImgHTMLAttributes<HTMLImageElement>)} />
+  ),
+};
+
+export const Blend: Story = {
+  args: {
+    src: image37Logo,
+  },
+  render: (args) => (
+    <BlendImage {...(args as React.ImgHTMLAttributes<HTMLImageElement>)} />
+  ),
+};
+
+export const Text: Story = {
+  args: {
+    src: image54,
+    text: 'coffee',
+  },
+  render: (args) => <LetterImage src={args.src}>{args.text}</LetterImage>,
+};
--- apps/css-playground/src/components/image/index.tsx
+++ apps/css-playground/src/components/image/index.tsx
@@ -1,10 +1,117 @@
 import styled from 'styled-components';
-import image12 from '~/assets/images/pic012.jpg';
+import Pic037 from '~/assets/images/pic037.jpg';
 
-const ShadowImage = styled.img`
+export const ShadowImage = styled.img`
   box-shadow: 15px 15px 0 #ea987e;
 `;
 
-export const FlowerImage = () => {
-  return <ShadowImage src={image12} />;
+const ShadeImageWrapper = styled.div`
+  position: absolute;
+
+  img {
+    position: relative;
+    z-index: 2;
+  }
+
+  ::before {
+    content: '';
+    position: absolute;
+    bottom: -15px;
+    right: -15px;
+    width: 100%;
+    height: 100%;
+    background-image: repeating-linear-gradient(
+      -45deg,
+      #d34e23 0px,
+      #d34e23 2px,
+      rgba(0 0 0 / 0) 0%,
+      rgba(0 0 0 / 0) 1%
+    );
+    z-index: 1;
+  }
+`;
+
+export const ShadeImage = (
+  props: React.ImgHTMLAttributes<HTMLImageElement>
+) => {
+  return (
+    <ShadeImageWrapper>
+      <img src={props.src} alt={props.alt} {...props} />
+    </ShadeImageWrapper>
+  );
 };
+
+const FrameImageWrapper = styled.div`
+  position: absolute;
+
+  ::before,
+  ::after {
+    content: '';
+    position: absolute;
+    transform: rotate(-35deg);
+    width: 70px;
+    height: 25px;
+    background-color: #ea987e;
+  }
+
+  ::before {
+    top: -10px;
+    left: -25px;
+  }
+
+  ::after {
+    bottom: -10px;
+    right: -25px;
+  }
+`;
+
+export const FrameImage = (
+  props: React.ImgHTMLAttributes<HTMLImageElement>
+) => {
+  return (
+    <FrameImageWrapper>
+      <img src={props.src} alt={props.alt} />
+    </FrameImageWrapper>
+  );
+};
+
+export const BlendImageBackground = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${Pic037});
+  background-size: auto 100%;
+  background-repeat: no-repeat;
+  background-position: center center;
+
+  height: 500px;
+  width: 500px;
+
+  img {
+    mix-blend-mode: multiply;
+  }
+`;
+
+export const BlendImage = (
+  props: React.ImgHTMLAttributes<HTMLImageElement>
+) => {
+  return (
+    <BlendImageBackground>
+      <img src={props.src} alt="" />
+    </BlendImageBackground>
+  );
+};
+
+export const LetterImage = styled.div<{ src: string }>`
+  background-clip: text;
+  -webkit-background-clip: text;
+
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-size: 100% cover;
+  color: rgba(0 0 0 / 0);
+  font-size: 200px;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
